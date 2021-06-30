c300c67735bdaaed0be574aedeb04588f458611d
Igor Suslikov <susia1981@gmail.com>
2021-06-30 12:46:17 +0300

    fixed any bags

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-
 // toggle mobile menu
 
 let mobileMenu = document.querySelector(".nav-mobile-menu");
 let mainMenu = document.querySelector(".row-headMenu");
 
-mobileMenu.addEventListener("click", function(){
-    mobileMenu.classList.toggle("active-menu");
-    if(mobileMenu.classList.contains("active-menu"))
-    {
-    mainMenu.classList.add("active-menu")
-    } 
-    else{
-        mainMenu.classList.remove("active-menu")
-    }
+mobileMenu.addEventListener("click", function () {
+  mobileMenu.classList.toggle("active-menu");
+  if (mobileMenu.classList.contains("active-menu")) {
+    mainMenu.classList.add("active-menu");
+  } else {
+    mainMenu.classList.remove("active-menu");
+  }
 });
 
 // slick slider
 
-    $('.slider-block').slick({
-        autoplay: true,
-        autoplaySpeed: 3000,
-        dots:true,
-
-
-
-    });
+".slider-block".slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+});
